8e44445e208cde9bafdbadbf6668cb8a524112a6
Jan Heinrich Reimer <heinrich@reimer.family>
2021-04-19 13:48:30 +0200

    Remove double comments

--- backend/backend.py
+++ backend/backend.py
@@ -27,8 +27,8 @@ modelIBM = Model("IBM.h5")
 modelIBM.label("Therefore fixed punishment will")
 
 modelCombo = Model("COMBO.h5")
-# # We must call this cause of a keras bug
-# # https://github.com/keras-team/keras/issues/2397
+# We must call this cause of a keras bug
+# https://github.com/keras-team/keras/issues/2397
 modelCombo.label("Therefore fixed punishment will")
 
 modelES = Model("ES.h5")
@@ -37,18 +37,18 @@ modelES = Model("ES.h5")
 modelES.label("Therefore fixed punishment will")
 
 modelWD = Model("WD.h5")
-# # We must call this cause of a keras bug
-# # https://github.com/keras-team/keras/issues/2397
+# We must call this cause of a keras bug
+# https://github.com/keras-team/keras/issues/2397
 modelWD.label("Therefore fixed punishment will")
 
 modelES_dep = Model("ES_dep.h5")
-# # We must call this cause of a keras bug
-# # https://github.com/keras-team/keras/issues/2397
+# We must call this cause of a keras bug
+# https://github.com/keras-team/keras/issues/2397
 modelES_dep.label("Therefore fixed punishment will")
 
 modelWD_dep = Model("WD_dep.h5")
-# # We must call this cause of a keras bug
-# # https://github.com/keras-team/keras/issues/2397
+# We must call this cause of a keras bug
+# https://github.com/keras-team/keras/issues/2397
 modelWD_dep.label("Therefore fixed punishment will")
 
 app = Flask(__name__)
